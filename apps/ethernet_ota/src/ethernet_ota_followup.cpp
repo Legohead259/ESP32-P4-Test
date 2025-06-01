d7a618de63553b9e8431e07604cfde9a1d086896
Braidan <legohead259@gmail.com>
2025-06-01 15:13:18 -0400

    Clean up OTA followup MVP applciation

--- apps/ethernet_ota/src/ethernet_ota_followup.cpp
+++ apps/ethernet_ota/src/ethernet_ota_followup.cpp
@@ -1,29 +1,12 @@
 #include <Arduino.h>
-#include <ETH.h>
-#include <ESPmDNS.h>
-#include <NetworkUdp.h>
-#include <ArduinoOTA.h>
 #include <Controller.h>
 
-//variabls for blinking an LED with Millis
-const int led = 2; // ESP32 Pin to which onboard LED is connected
-unsigned long previousMillis = 0;  // will store last time LED was updated
-const long interval = 1000;  // interval at which to blink (milliseconds)
-int ledState = LOW;  // ledState used to set the LED
-
 void setup() {
-
-    pinMode(led, OUTPUT);
-    
     Serial.begin(115200);
-    Serial.println("Booting");
+
     controller.begin();
 }
 
 void loop() {    
-    //loop to blink without delay
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval) {
-        Serial.println("Working...");   
-    }
+
 }
