d72d9da8d46da9d8eac0e88f5a35a91cfb5be742
Braidan <legohead259@gmail.com>
2025-06-01 15:13:06 -0400

    Implement ESP log instead of Serial.print logging

--- lib/controller/Controller.cpp
+++ lib/controller/Controller.cpp
@@ -1,8 +1,7 @@
 #include "Controller.h"
 
 Controller::Controller()
-    :   _network(),
-        // Configure IP options based on build flags
+    :   // Configure IP options based on build flags
         #ifdef CONTROLLER_ID
         #ifdef IP_DOMAIN_OCTET_0 // If octet 0 set, assume octet 1 and 2 are also set
         localIP(IP_DOMAIN_OCTET_0, IP_DOMAIN_OCTET_1, IP_DOMAIN_OCTET_2, CONTROLLER_ID),
@@ -20,47 +19,9 @@ Controller::Controller()
     {
 }
 
-void Controller::begin() {
-    // TODO: Replace with class network instance
-    Network.onEvent(std::bind(&Controller::_onNetworkEvent, this, std::placeholders::_1));
-    // TODO: Replace with class ETH instance
-    ETH.begin(ETH_PHY_TYPE, ETH_PHY_ADDR, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_POWER, ETH_CLK_MODE);
-    ETH.config(localIP, gateway, subnet);
-
-    while (!isEthConnected) {
-        Serial.print(".");
-        delay(100);
-    }
-
-
-    // TODO: Enable setting custom upload port
-    // Port defaults to 3232
-    // ArduinoOTA.setPort(3232);
-
-    // TODO: Enable setting custom upload hostname
-    // Hostname defaults to esp3232-[MAC]
-    // ArduinoOTA.setHostname("myesp32");
-
-    // TODO: Enable setting authentication
-    // No authentication by default
-    // ArduinoOTA.setPassword("admin");
-
-    // Password can be set with it's md5 value as well
-    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-    // TODO: Replace with class instance of ArduinoOTA
-    ArduinoOTA
-        .onStart(std::bind(&Controller::_onOTAStart, this))
-        .onEnd(std::bind(&Controller::_onOTAEnd, this))
-        .onProgress(std::bind(&Controller::_onOTAProgress, this, std::placeholders::_1, std::placeholders::_2))
-        .onError(std::bind(&Controller::_onOTAError, this, std::placeholders::_1));
-
-    ArduinoOTA.begin();
-
-    Serial.println("Ready");
-    Serial.print("IP address: ");
-    Serial.println(localIP.toString());
+bool Controller::begin() {
+    bool _isNetworkStarted = _beginNetwork();
+    bool _isOTAStarted = _beginOTA();
 
     xTaskCreate(
         otaTaskCallback,    // Function to implement the task
@@ -70,12 +31,67 @@ void Controller::begin() {
         1,                  // Priority of the task
         &arduinoOTATask     // Task handle
     );
+
+    return (_isNetworkStarted && _isOTAStarted);
+}
+
+bool Controller::_beginNetwork(long timeout) {
+    // Must use global Network object for some reason. Does not work if we define our own instance *shrug*
+    if (!Network.begin()) {
+        log(LOG_ERROR, "Network failed to start!");
+        return false;
+    }
+    log(LOG_INFO, "Network started");
+    Network.onEvent(std::bind(&Controller::_onNetworkEvent, this, std::placeholders::_1));  
+    
+    // Must use global ETH object for some reason. Does not work if we define our own instance *shrug*
+    bool _isEthStarted = ETH.begin(ETH_PHY_TYPE, ETH_PHY_ADDR, ETH_PHY_MDC, ETH_PHY_MDIO, ETH_PHY_POWER, ETH_CLK_MODE);
+    ETH.config(localIP, gateway, subnet);
+
+    // Check connection timeout
+    long _timeoutStart = millis();
+    long _timeoutEnd = _timeoutStart + timeout;
+    while (!isEthConnected) {
+        log(LOG_DEBUG, ".");
+        if (millis() >= _timeoutEnd) return false; // TODO: Replace with network start enum type
+    }
+
+    return true; // TODO: Replace with network start enum type
+}
+
+bool Controller::_beginOTA() {
+    // TODO: Enable setting custom upload port
+    // Port defaults to 3232
+    // ArduinoOTA.setPort(3232);
+
+    #ifdef CONTROLLER_HOSTNAME
+    ArduinoOTA.setHostname(CONTROLLER_HOSTNAME);
+    #else
+    ArduinoOTA.setHostname("esp-hostname");
+    #endif // DEF(CONTROLLER_HOSTNAME)
+
+    // TODO: Enable setting authentication
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    // Must use global ArduinoOTA object. Static callback for otaTaskCallback does not work with instance-specific ArduinoOTA object
+    ArduinoOTA
+        .onStart(std::bind(&Controller::_onOTAStart, this))
+        .onEnd(std::bind(&Controller::_onOTAEnd, this))
+        .onProgress(std::bind(&Controller::_onOTAProgress, this, std::placeholders::_1, std::placeholders::_2))
+        .onError(std::bind(&Controller::_onOTAError, this, std::placeholders::_1));
+
+    ArduinoOTA.begin();
+    return true; // Currently no logic to check if OTA is active or not.
 }
 
 void Controller::_onNetworkEvent(arduino_event_id_t event) {
     switch (event) {
         case ARDUINO_EVENT_ETH_START:
-            Serial.println("ETH Started");
             // The hostname must be set after the interface is started, but needs
             // to be set before DHCP, so set it from the event handler thread.
             #ifdef CONTROLLER_HOSTNAME
@@ -83,25 +99,28 @@ void Controller::_onNetworkEvent(arduino_event_id_t event) {
             #else
             ETH.setHostname("esp-hostname");
             #endif // DEF(CONTROLLER_HOSTNAME)
+            logf(LOG_INFO, "ETH Started\n\r\tHostname: %s", ETH.getHostname());
             break;
         case ARDUINO_EVENT_ETH_CONNECTED: 
-            Serial.println("ETH Connected"); 
+            log(LOG_INFO, "ETH Connected");
             break;
         case ARDUINO_EVENT_ETH_GOT_IP:
-            Serial.println("ETH Got IP");
-            Serial.println(ETH);
+            logf(LOG_INFO, "ETH Got IP\n\r\tIP Address: %s \r\n\tSubnet: %s\r\n\tGateway: %s", 
+                ETH.localIP().toString().c_str(),
+                ETH.subnetMask().toString().c_str(),
+                ETH.gatewayIP().toString().c_str());
             isEthConnected = true;
             break;
         case ARDUINO_EVENT_ETH_LOST_IP:
-            Serial.println("ETH Lost IP");
+            log(LOG_WARN, "ETH Lost IP");
             isEthConnected = false;
             break;
         case ARDUINO_EVENT_ETH_DISCONNECTED:
-            Serial.println("ETH Disconnected");
+            log(LOG_WARN, "ETH Disconnected");
             isEthConnected = false;
             break;
         case ARDUINO_EVENT_ETH_STOP:
-            Serial.println("ETH Stopped");
+            log(LOG_ERROR, "ETH Stopped");
             isEthConnected = false;
             break;
         default: break;
@@ -116,24 +135,28 @@ void Controller::_onOTAStart() {
         type = "filesystem";
 
     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
+    logf(LOG_INFO, "Start OTA updating - %s", type);
 }
 
 void Controller::_onOTAEnd() {
-    Serial.println("\nEnd");
+    log(LOG_INFO, "OTA End");
 }
 
 void Controller::_onOTAProgress(unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    logf(LOG_INFO, "OTA Progress: %u%%\r", (progress / (total / 100)));
 }
 
 void Controller::_onOTAError(ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    char error_str[16];
+    switch(error) {
+        case OTA_AUTH_ERROR: strcpy(error_str, "Auth Failed"); break;
+        case OTA_BEGIN_ERROR: strcpy(error_str, "Begin Failed"); break;
+        case OTA_CONNECT_ERROR: strcpy(error_str, "Connect Failed"); break;
+        case OTA_RECEIVE_ERROR: strcpy(error_str, "Receive Failed"); break;
+        case OTA_END_ERROR: strcpy(error_str, "End Failed"); break;
+        default: strcpy(error_str, "Check OTA docs"); break;
+    }
+    logf(LOG_ERROR, "OTA Error[%u]: %s", error, error_str);
 }
 
 Controller controller;
--- lib/controller/Controller.h
+++ lib/controller/Controller.h
@@ -1,15 +1,73 @@
 #include <ETH.h>
 #include <ArduinoOTA.h>
+#include <esp_log.h>
+
+typedef enum {
+    LOG_SILENT,
+    LOG_ERROR,
+    LOG_WARN,
+    LOG_INFO,
+    LOG_DEBUG,
+    LOG_VERBOSE
+} controller_log_level_t;
 
 class Controller {
 public:
     Controller();
     ~Controller() {}
 
-    void begin();
+    bool begin();
+    
+    void setLogLevel(controller_log_level_t level) { currentLogLevel = level; }
+
+    controller_log_level_t getLogLevel() { return currentLogLevel; }
+
+protected:
+    static constexpr const char* TAG = "Controller";
+
+    virtual void log(controller_log_level_t level, const char* message) {
+        if (level <= currentLogLevel) {
+            esp_log_level_t espLevel = convertToEspLogLevel(level);
+            switch (level) {
+                case ESP_LOG_ERROR:     ESP_LOGE(TAG, "%s", message); break;
+                case ESP_LOG_WARN:      ESP_LOGW(TAG, "%s", message); break;
+                case ESP_LOG_INFO:      ESP_LOGI(TAG, "%s", message); break;
+                case ESP_LOG_DEBUG:     ESP_LOGD(TAG, "%s", message); break;
+                case ESP_LOG_VERBOSE:   ESP_LOGV(TAG, "%s", message); break;
+                default:                break;
+            }
+        }
+    }
+
+    virtual void logf(controller_log_level_t level, const char* format, ...) {
+        if (level <= currentLogLevel) {
+            char buffer[1024];
+            va_list args;
+            va_start(args, format);
+            vsnprintf(buffer, sizeof(buffer), format, args);
+            log(level, buffer);
+            va_end(args);
+        }
+    }
 
 private:
-    NetworkManager _network;
+    controller_log_level_t currentLogLevel = LOG_INFO;
+
+    esp_log_level_t convertToEspLogLevel(controller_log_level_t level) {
+        switch(level) {
+            case LOG_ERROR:     return ESP_LOG_ERROR;
+            case LOG_WARN:      return ESP_LOG_WARN;
+            case LOG_INFO:      return ESP_LOG_INFO;
+            case LOG_DEBUG:     return ESP_LOG_DEBUG;
+            case LOG_VERBOSE:   return ESP_LOG_VERBOSE;
+            default:            return ESP_LOG_NONE;
+        }
+    }
+
+    void espLogf(esp_log_level_t level, const char* format, va_list args) {
+        
+    }
+
     IPAddress localIP;
     IPAddress gateway;
     IPAddress subnet;
@@ -17,6 +75,9 @@ private:
     TaskHandle_t arduinoOTATask;
 
     bool isEthConnected;
+
+    bool _beginNetwork(long timeout=10000);
+    bool _beginOTA();
 
     void _onNetworkEvent(arduino_event_id_t event);
 
@@ -28,6 +89,7 @@ private:
     static void otaTaskCallback(void* pvParameters) {
         for (;;) {
             ArduinoOTA.handle();
+            vTaskDelay(pdMS_TO_TICKS(10));  // Yield to scheduler every 10 ms
         }
     }
 };
